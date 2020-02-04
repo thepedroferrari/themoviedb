5b6be6cc0b6c63a7351c797d5a2500b28ff35bf0
Pedro Ferrari <pedro@pedroferrari.com>
2020-02-04 07:43:17 +0100

    Setup Apollo with GraphQL

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,7 +1,32 @@
+import { ApolloServer, gql } from 'apollo-server-micro'
 import { NextApiResponse, NextApiRequest } from "next"
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({
-    test: 'SUUUP!PPPP M8'
-  })
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    sayHello: () => {
+      return 'Haaai'
+    }
+  }
 }
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+export default apolloServer.createHandler({ path: '/api/graphql' })
+
+// export default (req: NextApiRequest, res: NextApiResponse) => {
+//   res.status(200).json({
+//     test: 'SUUUP!PPPP M8'
+//   })
+// }
